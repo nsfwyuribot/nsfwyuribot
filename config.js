aae0148311229b53ac760a04666a86c75b71e911
nsfwyuribot <46269534+nsfwyuribot@users.noreply.github.com>
2018-12-30 14:37:18 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+	consumer_key:         'FTiIeQEpZ3hMyz7zyYhxoYNVg',
+	consumer_secret:      'eueEiPx1VV08HR3ZIdaxJd7YQgH0RPapb0Yta33AoLyJ3vYRIe',
+	access_token:         '1079211382086688768-GFxeMBWByUqluq9Uv0oJ85xgC2xOl3',
+	access_token_secret:  'Y8Cqb2Z0KTzHVoQ3UtyVwYwP2E47m1k68J1t8cd9DQl0x'
+}
+
+module.exports = config;
